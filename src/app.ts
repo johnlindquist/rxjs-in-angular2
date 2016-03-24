bbcb61c07259a1840b63c0b43b68bc0506b39dc1
johnlindquist <johnlindquist@gmail.com>
2016-03-23 18:37:20 -0600

    03

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,21 @@ import {Component} from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
+import {Subject} from "rxjs/Subject";
 
 @Component({
     selector: 'app',
     template: `
+        <button (click)="click$.next()">Update</button>
         <h1>{{clock | async | date:'yMMMMEEEEdjms'}}</h1>
         `
 })
 export class App {
-    clock = Observable
-        .interval(1000)
-        .map(()=> new Date());
+    click$ = new Subject();
+
+    clock;
+
+    constructor(){
+      this.clock = this.click$.map(()=> new Date());
+    }
 }
